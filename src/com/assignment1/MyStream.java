d9ae20b68ea9837a304f0bb9a9196544de7345d4
Dev Gupta <devgupta097@gmail.com>
2023-07-16 21:24:40 +0530

    removed unused imports

--- src/com/assignment1/MyStream.java
+++ src/com/assignment1/MyStream.java
@@ -7,40 +7,40 @@ import java.util.function.Predicate;
 import java.util.stream.Collector;
 
 public class MyStream<T> {
-	
+
 	private List<T> elements;
-	
+
 	private MyStream(List<T> elements) {
-        this.elements = elements;
-    }
+		this.elements = elements;
+	}
 	
 	public static <T> MyStream<T> of(List<T> elements) {
-        return new MyStream<>(elements);
+		return new MyStream<>(elements);
     }
 
 	public MyStream<T> filter(Predicate<T> predicate) {
 		List<T> filteredElements = new ArrayList<>();
-        for (T element : elements) {
-            if (predicate.test(element)) {
-                filteredElements.add(element);
-            }
-        }
-        return new MyStream<>(filteredElements);
+		for (T element : elements) {
+			if (predicate.test(element)) {
+				filteredElements.add(element);
+			}
+		}
+		return new MyStream<>(filteredElements);
 	}
-
+	
 	public <R> MyStream<R> map(Function<T,R> mapper) {
 		List<R> mappedElements = new ArrayList<>();
-        for (T element : elements) {
-            mappedElements.add(mapper.apply(element));
-        }
-        return new MyStream<>(mappedElements);
+		for (T element : elements) {
+			mappedElements.add(mapper.apply(element));
+		}
+		return new MyStream<>(mappedElements);
 	}
-
+	
 	public <R, A> R collect(Collector<? super T, A, R> collector) {
-        A accumulator = collector.supplier().get();
-        for (T element : elements) {
-            collector.accumulator().accept(accumulator, element);
-        }
-        return collector.finisher().apply(accumulator);
-    }
+		A accumulator = collector.supplier().get();
+		for (T element : elements) {
+			collector.accumulator().accept(accumulator, element);
+		}
+		return collector.finisher().apply(accumulator);
+	}
 }
